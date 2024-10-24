3177afe1c913827993cfac71d2035b81cd248635
Lxs_ <Calebgirard2008@gmail.com>
2024-10-24 13:05:20 -0400

    update

--- maintest.py
+++ maintest.py
@@ -1,7 +1,14 @@
-import JsonSaving as js
+import JsonHelper as js
 
 savemanager = js.JsonManager()
 
 savemanager.open("data.json")
-savemanager.ListValue("list1",0,2)
-savemanager.Value("list2","wow")
+savemanager.Value("val","wow")
+savemanager.Dict("dict",{"plane":1})
+savemanager.DictValue("dict","plane",True)
+savemanager.List("list",[0,1])
+savemanager.ListValue("list",1,True)
+savemanager.ListValue("list",0,"It works")
+savemanager.reset()
+savemanager.open("data2.json")
+savemanager.Value("val",0)
