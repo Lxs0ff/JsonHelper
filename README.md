18c8087eccd64c2f96165f7ff3320f933e458951
Lxs_ <Calebgirard2008@gmail.com>
2024-10-24 16:02:29 -0400

    update example

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# JsonSaving
-# to use, drop in the file JsonHelper.py inside the same folder as your python script and write "import JsonHelper"
-# then "variable_name = JsonHelper.JsonManager()"
+# JsonHelper
+- to use, drop in the file JsonHelper.py inside the same folder as your python script and write "import JsonHelper"
+- for an example on how to use it go see the script example.py
--- example.py
+++ example.py
@@ -1,29 +1,41 @@
+# Before using this tool make sure that you downloaded the script JsonHelper.py and put it in the same folder as the script that is using this tool
+
+# import the tool
 import JsonHelper as JH
 
+# Create a JsonManager
 save = JH.JsonManager()
+
+# Open or Create a .json and load it's contents into the JsonManager
 save.open("example-data.json")
-name = input("Enter your name: ")
-save.Dict(name,{"age":None,"favorite_color":None,"job":None})
 
-try:
-    save.Value("#ofpeople",save.getValue("#ofpeople")+1)
-except:
-    save.Value("#ofpeople",0)
-    
-try:
-    nameidx = len(save.getValue("names"))
-    save.ListValue("names",len(save.getValue("names")),name)
-except:
-    save.List("names",[name])
-    nameidx = 0
+# Change the value of a key
+save.Value("val","wow")
 
-save.DictValue(name,"favorite_color",input("Enter your favorite color: "))
-save.DictValue(name,"job",input("Enter your job: "))
+# Set the value of a key to a dictionary 
+save.Dict("dict",{"plane":1})
 
-try:
-    save.DictValue(name,"age",int(input("Enter your age: ")))
-except:
-    save.Dict(name,{"age":None,"favorite_color":None,"job":None})
-    save.Value("#ofpeople",save.getValue("#ofpeople")-1)
-    save.List("names",save.getValue("names").pop(nameidx-1))
-    print("Your age must be a number only")
+# Change a value for a key inside the selected dictionary
+save.DictValue("dict","plane",True)
+
+# Set the value for a key to a list
+save.List("list",[0,1])
+
+# Change a value at an index position inside the selected list
+save.ListValue("list",1,True)
+save.ListValue("list",0,"It works")
+
+# Save.getValue("test-key") gets the value at the provided key
+print("Value at key 'val' for save#1: "+str(save.getValue("val")))
+
+# Resets the JsonManager so no file is opened
+save.reset()
+
+# Because we reseted the JsonManager, we must re-open a .json file to continue using functions like Value, Dict, List, getValue, ect...
+save.open("example-data-2.json")
+
+# Change the value of a key
+save.Value("val",0)
+
+# Save.getValue("test-key") gets the value at the provided key
+print("Value at key 'val' for save#2: "+str(save.getValue("val")))
